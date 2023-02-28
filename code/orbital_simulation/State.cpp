d813842c482ec9a33ab139b37771263f24ac9fad
jambox <nonya@business.com>
2023-02-28 13:29:01 -0800

    adding debug mechanisms for mechanics

--- code/orbital_simulation/State.cpp
+++ code/orbital_simulation/State.cpp
@@ -21,10 +21,10 @@ void State::SetAccel(enum ACCEL_MODE mode, v2d acc) {
 
 void State::UpdateState(){
   // update velocity
-  // std::cout << "init vel: " << vel_ << "\r\n";
+  std::cout << "init vel: " << vel_ << "\r\n";
   vel_ += vel_ + State::DELTA_T * acc_; // could use chrono for delta t calc...
-  // std::cout << "update vel: " << vel_ << "\r\n";
-  // std::cout << "init pos: " << pos_ << "\r\n";
-  pos_ += 0.5 * pow(State::DELTA_T, 2)* acc_  + State::DELTA_T * vel_;
+  std::cout << "update vel: " << vel_ << "\r\n";
+  std::cout << "init pos: " << pos_ << "\r\n";
+  pos_ += State::DELTA_T * vel_;
   std::cout << "update pos: " << pos_ << "\r\n";
 }
